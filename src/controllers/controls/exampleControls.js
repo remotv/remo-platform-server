95c37eba37fd2370c50b70749aa496928a3535a5
Jillian Ogle <jill@letsrobot.tv>
2020-03-30 15:01:44 -0700

    generate unique id's for example controls

--- src/controllers/controls/exampleControls.js
+++ src/controllers/controls/exampleControls.js
@@ -1,22 +1,29 @@
 module.exports = () => {
-  return [
-    { break: "line", label: "movement", id: "1" },
-    { label: "forward", hot_key: "w", command: "f", id: "2" },
-    { label: "back", hot_key: "s", command: "b", id: "3" },
-    { label: "left", hot_key: "a", command: "l", id: "4" },
-    { label: "right", hot_key: "d", command: "r", id: "5" },
-    { break: "line", label: "", id: "6" },
+  const { makeId } = require("../../modules/utilities");
+  const exampleControls = [
+    { break: "line", label: "movement" },
+    { label: "forward", hot_key: "w", command: "f" },
+    { label: "back", hot_key: "s", command: "b" },
+    { label: "left", hot_key: "a", command: "l" },
+    { label: "right", hot_key: "d", command: "r" },
+    { break: "line", label: "" },
     {
       label: "example admin command",
       command: "example",
-      access: "owner",
-      id: "7"
+      access: "owner"
     },
     {
       label: "example timer",
       command: "timer-example",
-      id: "8",
       cooldown: 100
     }
   ];
+
+  let exportExample = [];
+  exampleControls.forEach(button => {
+    button.id = `bttn-${makeId()}`;
+    exportExample.push(button);
+  });
+
+  return exportExample;
 };
