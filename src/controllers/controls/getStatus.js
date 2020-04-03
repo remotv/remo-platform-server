c8ec6143015f25a104e4244c2256e11ce06faf52
Jillian Ogle <jill@letsrobot.tv>
2020-04-03 13:34:07 -0700

    remove console logs

--- src/controllers/controls/getStatus.js
+++ src/controllers/controls/getStatus.js
@@ -7,16 +7,16 @@ module.exports.getStatusForControls = async buttons => {
       return await this.getButtonStatus(button);
     };
     appendStatus = await Promise.all(buttons.map(button => getStatus(button)));
-    console.log("Status Check: ", appendStatus);
+    // console.log("Status Check: ", appendStatus);
     return appendStatus;
   }
-  console.log("Input required for getStatusForControls()");
+  // console.log("Input required for getStatusForControls()");
   return null;
 };
 
 module.exports.getButtonStatus = async button => {
   const { getButtonTimer } = require("./buttonTimers");
-  console.log("Check Button Status: ", button);
+  // console.log("Check Button Status: ", button);
   let check = null;
   if (button.cooldown) check = await getButtonTimer(button.id);
   if (check) return check;
