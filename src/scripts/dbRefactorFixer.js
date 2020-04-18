19a5909961a199d4f5ca67fe1225579b8454a9ad
Jillian Ogle <jill@letsrobot.tv>
2020-04-18 14:33:44 -0700

    refactor combine robot & channels model

--- src/scripts/dbRefactorFixer.js
+++ src/scripts/dbRefactorFixer.js
@@ -17,9 +17,11 @@ const run = async () => {
 
     const robots = await getAllRobots();
     const channels = await getAllChannels();
+
     console.log(
       "Total Robots: ",
       robots.length,
+      "\n",
       "Total Channels: ",
       channels.length
     );
@@ -31,24 +33,31 @@ const run = async () => {
     });
 
     const checkChannels = await channels.map(async (channel) => {
+      let robotFound = false;
       robotsWithLinkedChannels.map((robot) => {
         if (robot.status.current_channel === channel.id) {
-          console.log("Combine");
-        } else {
-          channelsWithNoRobot.push(channel);
+          console.log("Combine", `${robot.name} + ${channel.name}`);
+          robot.channel_info = channel;
+          robotFound = true;
+          return;
         }
       });
+      if (!robotFound) channelsWithNoRobot.push(channel);
     });
 
     await Promise.all(checkRobots, checkChannels);
     console.log(
       "Robots With Linked Channels: ",
       robotsWithLinkedChannels.length,
+      "\n",
       "Robots without a Linked Channels: ",
       robotsWithNoChannels.length,
+      "\n",
       "Channels without Linked Robots: ",
       channelsWithNoRobot.length
     );
+
+    await buildRobotChannels(robotsWithLinkedChannels);
   } catch (err) {
     console.log(err);
   }
@@ -65,6 +74,50 @@ const getAllRobots = async () => {
     console.log(err);
   }
   return [];
+};
+
+buildRobotChannels = async (linkedRobots, unlinkedRobots, unlinkedChannels) => {
+  let robotChannels = [];
+  const combineLinkedRobots = linkedRobots.map((robot) => {
+    const combine = makeRobotChannel({
+      name: robot.name,
+      id: robot.id,
+      server_id: robot.host_id,
+      owner_id: robot.owner_id,
+      chat_id: robot.channel_info.chat,
+      created: robot.created,
+      controls: robot.channel_info.controls,
+      heartbeat: robot.heartbeat,
+    });
+    console.log(combine);
+    robotChannels.push(combine);
+  });
+
+  await Promise.all(combineLinkedRobots);
+};
+
+makeRobotChannel = ({
+  name,
+  id,
+  server_id,
+  owner_id,
+  chat_id,
+  created,
+  controls,
+  heartbeat,
+}) => {
+  const { makeId } = require("../modules/utilities");
+  return {
+    name: name || "",
+    id: id || `rbot-${makeId()}`,
+    type: "robot_channel",
+    server_id: server_id,
+    owner_id: owner_id || "",
+    chat_id: chat_id || "",
+    created: created || Date.now(),
+    controls: controls || "",
+    heartbeat: heartbeat || 0,
+  };
 };
 
 run();
