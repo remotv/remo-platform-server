8766fc0d99af52541fdf56aa916c92b4f953a996
Jillian Ogle <jill@letsrobot.tv>
2020-04-20 13:41:31 -0700

    robot_channel model

--- src/models/robotChannels/index.js
+++ src/models/robotChannels/index.js
@@ -1,3 +1,20 @@
+/**
+ * Robot Channels:
+ *  unique parameters:
+ *  @param {string} name : name of channel
+ *  @param {string} id : unique id
+ *  @param {string} type : "robot_channel"
+ *  @param {int} created : timestamp when channel was created, in millis
+ *  @param {int} heartbeat : timestamp of when robot was last online, in millis
+ *
+ *  reference IDs:
+ *  @param {string} server_id server this channel is contained within
+ *  @param {string} owner_id owner of the robot / channel
+ *  @param {string} chat_id chat displayed for channel
+ *  @param {string} controls_id controls displayed on channel
+ *
+ */
+
 module.exports = {
   saveRobotChannel: require("./saveRobotChannel"),
   log: require("./log"),
