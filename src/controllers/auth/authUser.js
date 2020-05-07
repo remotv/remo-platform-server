29f6e19241183e2c0a704e90af91710fdfde7975
Jillian Ogle <jill@letsrobot.tv>
2020-05-07 12:18:22 -0700

    split token extraction and validating token data into two functions

--- src/controllers/auth/authUser.js
+++ src/controllers/auth/authUser.js
@@ -1,30 +1,13 @@
 module.exports = async (token) => {
-  const { extractToken } = require("./extractToken");
-  const { getUserInfoFromid } = require("../../models/user");
-  const { log } = require("./");
+  const { extractToken, authUserData, log } = require("./");
+  //extract token, validate data, and return authorized user or null
   try {
-    //extract data from token:
+    log(`Auth user w/ Token: 
+      ${token}`);
     const tokenData = await extractToken(token);
-
-    //require data
-    if (!tokenData) throw Error(`Unable to extract data from token ${token}`);
-    if (tokenData && !tokenData.id)
-      throw Error(`Token { id } required, ${tokenData}`);
-
-    //get user w/ token data:
-    const user = await getUserInfoFromid(tokenData.id);
-    if (!user) throw Error(`Unable to get user data from token: ${tokenData}`);
-    if (user && user.id && user.id !== tokenData.id)
-      throw Error(`Invalid Token Data for ID: ${tokenData.id}`);
-
-    //if session data is present, validate session data as well
-    if (
-      (user.session_id && !tokenData.session_id) ||
-      user.session_id !== tokenData.session_id
-    )
-      throw Error(`Invalid token data for session: ${tokenData.session_id}`);
-
-    //return user from DB on success, else log error and return null
+    if (!tokenData) throw Error(`Unable to extract token from data`);
+    const user = await authUserData(tokenData);
+    if (!user) throw Error(`Unable to authorize user w/ token data`);
     return user;
   } catch (err) {
     log(err.message);
---
+++ src/controllers/auth/authUserData.js
@@ -0,0 +1,29 @@
+module.exports = async (tokenData) => {
+  const { getUserInfoFromid } = require("../../models/user");
+  const { log } = require(".");
+  try {
+    //require data
+    if (!tokenData) throw Error(`Unable to extract data from token ${token}`);
+    if (tokenData && !tokenData.id)
+      throw Error(`Token { id } required, ${tokenData}`);
+
+    //get user w/ token data:
+    const user = await getUserInfoFromid(tokenData.id);
+    if (!user) throw Error(`Unable to get user data from token: ${tokenData}`);
+    if (user && user.id && user.id !== tokenData.id)
+      throw Error(`Invalid Token Data for ID: ${tokenData.id}`);
+
+    //if session data is present, validate session data as well
+    if (
+      (user.session_id && !tokenData.session_id) ||
+      user.session_id !== tokenData.session_id
+    )
+      throw Error(`Invalid token data for session: ${tokenData.session_id}`);
+
+    //return user from DB on success, else log error and return null
+    return user;
+  } catch (err) {
+    log(err.message);
+  }
+  return null;
+};
