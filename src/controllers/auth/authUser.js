6972826957182c2d05723d3d7034a931acdf1f25
Jillian Ogle <jill@letsrobot.tv>
2020-05-07 12:07:40 -0700

    better error messages for authUser.js

--- src/controllers/auth/authUser.js
+++ src/controllers/auth/authUser.js
@@ -7,22 +7,22 @@ module.exports = async (token) => {
     const tokenData = await extractToken(token);
 
     //require data
-    if (!tokenData) throw Error("Unable to extract data from token");
+    if (!tokenData) throw Error(`Unable to extract data from token ${token}`);
     if (tokenData && !tokenData.id)
-      throw Error("Invalid token data, must contain element, id");
+      throw Error(`Token { id } required, ${tokenData}`);
 
     //get user w/ token data:
     const user = await getUserInfoFromid(tokenData.id);
-    if (!user) throw Error("Unable to get user data from token.");
+    if (!user) throw Error(`Unable to get user data from token: ${tokenData}`);
     if (user && user.id && user.id !== tokenData.id)
-      throw Error("Invalid token data for id");
+      throw Error(`Invalid Token Data for ID: ${tokenData.id}`);
 
     //if session data is present, validate session data as well
     if (
       (user.session_id && !tokenData.session_id) ||
       user.session_id !== tokenData.session_id
     )
-      throw Error("Invalid token data for session");
+      throw Error(`Invalid token data for session: ${tokenData.session_id}`);
 
     //return user from DB on success, else log error and return null
     return user;
