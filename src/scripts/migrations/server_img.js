76df614028ff838f9eb1ae342d55bfa7d85d8f11
Jillian Ogle <jill@letsrobot.tv>
2020-05-22 15:32:42 -0700

    images table migration script

---
+++ src/scripts/migrations/server_img.js
@@ -0,0 +1,21 @@
+const run = async () => {
+  const db = require("../../services/db");
+  const update = `
+  CREATE TABLE public.images (
+   id character varying NOT NULL PRIMARY KEY,
+   user_id character varying,
+   created timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+   approved boolean
+);
+    `;
+  try {
+    console.log("Updating Table...");
+    const result = await db.query(update);
+    console.log(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+run();
