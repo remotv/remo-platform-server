11bb27d8b7ba1a4c731d48a98a65fd04e8a7889c
Jillian Ogle <jill@letsrobot.tv>
2020-06-12 13:59:19 -0700

    check for internal listener

--- src/events/internalApproveImage.js
+++ src/events/internalApproveImage.js
@@ -3,21 +3,23 @@ module.exports = async (ws, data) => {
   const { approveImage, deleteImage } = require("../models/images");
   const wss = require("../services/wss");
   let result = [];
-  try {
-    if (data.approved) {
-      const approveImg = await approveImage(data);
-      const updateServer = await updateServerImage({
-        server_id: data.ref,
-        image_id: data.id,
-      });
-      result.push(approveImg, updateServer);
-    } else {
-      const removeImg = await deleteImage(data.id);
-      result.push(removeImg);
+  if (ws.internalListener) {
+    try {
+      if (data.approved) {
+        const approveImg = await approveImage(data);
+        const updateServer = await updateServerImage({
+          server_id: data.ref,
+          image_id: data.id,
+        });
+        result.push(approveImg, updateServer);
+      } else {
+        const removeImg = await deleteImage(data.id);
+        result.push(removeImg);
+      }
+    } catch (err) {
+      console.log(err);
+      result.push({ error: err });
     }
-  } catch (err) {
-    console.log(err);
-    result.push({ error: err });
+    wss.emitInternalEvent("INTERNAL_APPROVE_IMG_RESULT", result);
   }
-  wss.emitInternalEvent("INTERNAL_APPROVE_IMG_RESULT", result);
 };
