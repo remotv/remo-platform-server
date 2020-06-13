e875817d3ad74e33827261921140acb7b28e850e
Jillian Ogle <jill@letsrobot.tv>
2020-06-13 13:24:10 -0700

    Removing delete image since it's not actually deleting the image. Will make a separate handler for that.

--- src/events/internalApproveImage.js
+++ src/events/internalApproveImage.js
@@ -1,25 +1,21 @@
 module.exports = async (ws, data) => {
   const { updateServerImage } = require("../models/robotServer");
-  const { approveImage, deleteImage } = require("../models/images");
+  const { approveImage } = require("../models/images");
   const wss = require("../services/wss");
   let result = [];
-  if (ws.internalListener) {
-    try {
-      if (data.approved) {
-        const approveImg = await approveImage(data);
-        const updateServer = await updateServerImage({
-          server_id: data.ref,
-          image_id: data.id,
-        });
-        result.push(approveImg, updateServer);
-      } else {
-        const removeImg = await deleteImage(data.id);
-        result.push(removeImg);
-      }
-    } catch (err) {
-      console.log(err);
-      result.push({ error: err });
+
+  try {
+    if (ws.internalListener) {
+      const approveImg = await approveImage(data);
+      const updateServer = await updateServerImage({
+        server_id: data.ref,
+        image_id: data.id,
+      });
+      result.push(approveImg, updateServer);
+      wss.emitInternalEvent("INTERNAL_APPROVE_IMG_RESULT", result);
     }
-    wss.emitInternalEvent("INTERNAL_APPROVE_IMG_RESULT", result);
+  } catch (err) {
+    console.log(err);
+    result.push({ error: err });
   }
 };
