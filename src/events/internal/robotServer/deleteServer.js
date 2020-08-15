f9e9a1cb2636b5efec90c40917cf1c379508caa5
Jillian Ogle <jill@letsrobot.tv>
2020-08-15 14:28:51 -0700

    Internal Delete Robot Server event

---
+++ src/events/internal/robotServer/deleteServer.js
@@ -0,0 +1,18 @@
+module.exports = async (ws, data) => {
+  const { deleteRobotServer } = require("../../../models/robotServer");
+  const wss = require("../../../services/wss");
+  const { server_id } = data;
+  try {
+    console.log("MODERATION - Deleting Robot Server: ", server_id);
+    if (ws.internalListener) {
+      const remove = await deleteRobotServer(server_id);
+      wss.emitInternalEvent("DELETE_ROBOT_SERVER", {
+        server_id: server_id,
+        result: remove,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  //deleteServer
+};
