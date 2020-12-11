be8e4c95325b38f4c2bc3acddba2f906c0db8c4d
Jillian Ogle <jill@letsrobot.tv>
2020-12-10 16:14:52 -0800

    Up button limit to 128

--- src/config/index.js
+++ src/config/index.js
@@ -24,7 +24,7 @@ const defaults = {
   sendGrid: "",
   sendMail: "",
   enableEmailAlerts: true,
-  maxButtons: 96,
+  maxButtons: 128,
 
   urlPrefix: "https://remo.tv/",
   supportEmail: "jill@remo.tv",
